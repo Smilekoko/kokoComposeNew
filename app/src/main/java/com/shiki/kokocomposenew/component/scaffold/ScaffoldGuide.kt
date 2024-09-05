1c31b443361daa9eb3a307d76c847a74ffb19c91
koko <179621228@qq.com>
2024-09-06 00:22:41 +0800

    。。。

---
+++ app/src/main/java/com/shiki/kokocomposenew/component/scaffold/ScaffoldGuide.kt
@@ -0,0 +1,61 @@
+package com.shiki.kokocomposenew.component.scaffold
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shiki.kokocomposenew.R
+
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun ScaffoldGuide() {
+    var presses by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = Color(0xFFFFB7E0),
+                    titleContentColor = Color(0xFF5E8FFF),
+                ),
+                title = {
+                    Text(text = "TopAppBar")
+                })
+        },
+        content = ({ paddingValues ->
+
+        }),
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ){
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+
+        }
+
+    )
+}
