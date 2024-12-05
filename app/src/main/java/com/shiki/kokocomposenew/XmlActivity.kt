1e0a83aeaba1aa9bb9330f19c79882249168570e
smilekoko <179621228@qq.com>
2024-12-05 11:46:14 +0800

    ...结合xml和compose

---
+++ app/src/main/java/com/shiki/kokocomposenew/XmlActivity.kt
@@ -0,0 +1,30 @@
+package com.shiki.kokocomposenew
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.shiki.kokocomposenew.databinding.ActivityXmlBinding
+import com.shiki.kokocomposenew.text.QinyuanSpringSnowColumn
+
+class XmlActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityXmlBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityXmlBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.composeView.setContent {
+            QinyuanSpringSnowColumn()
+        }
+    }
+}
