9fca5b23b6c73468bd45d2b498d9eeffd64ccdee
smilekoko <179621228@qq.com>
2025-02-26 06:29:22 +0000

    ...

--- app/src/main/java/com/shiki/kokocomposenew/dialog/AlertDialogGuide.kt
+++ app/src/main/java/com/shiki/kokocomposenew/dialog/AlertDialogGuide.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -14,30 +16,39 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun AlertDialogExample() {
 
-    AlertDialog(
+    val openAlertDialog = remember { mutableStateOf(true) }
 
-        icon = {
-            Icon(Icons.Default.Favorite, contentDescription = "Favorite Icon")
-        },
-        title = {
-            Text(text = "AlertDialog")
-        },
-        text = {
-            Text(text = "This is an example of an alert dialog with buttons.")
-        },
-        onDismissRequest = {
+    if (openAlertDialog.value) {
+        AlertDialog(
 
-        },
-        confirmButton = {
-            TextButton(onClick = {}) {
-                Text("Confirm")
+            icon = {
+                Icon(Icons.Default.Favorite, contentDescription = "Favorite Icon")
+            },
+            title = {
+                Text(text = "AlertDialog")
+            },
+            text = {
+                Text(text = "This is an example of an alert dialog with buttons.")
+            },
+            onDismissRequest = {
+
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    openAlertDialog.value = false
+                }) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    openAlertDialog.value = false
+                }) {
+                    Text("Dismiss")
+                }
             }
-        },
-        dismissButton = {
-            TextButton(onClick = {}) {
-                Text("Dismiss")
-            }
-        }
-    )
+        )
+    }
+
 
 }
