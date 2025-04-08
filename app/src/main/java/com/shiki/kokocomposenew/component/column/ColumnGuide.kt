4d7758fa26ff21bc80537e1b795619f1967498f5
smilekoko <179621228@qq.com>
2025-04-08 18:30:50 +0800

    ...

--- app/src/main/java/com/shiki/kokocomposenew/component/column/ColumnGuide.kt
+++ app/src/main/java/com/shiki/kokocomposenew/component/column/ColumnGuide.kt
@@ -2,7 +2,9 @@ package com.shiki.kokocomposenew.component.column
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -10,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Preview
@@ -22,6 +25,22 @@ fun ColumnExample(messages: List<String> = listOf("koko", "koko")) {
     ) {
         messages.forEach {
             Text(it)
+        }
+    }
+
+
+}
+
+@Preview
+@Composable
+fun RowExample(messages: List<String> = listOf("koko", "koko", "koko")) {
+    Row(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+    ) {
+        messages.forEach {
+            Text(modifier = Modifier.padding(5.dp), text = it)
         }
     }
 
