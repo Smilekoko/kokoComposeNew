ac8cc185031816fb01d25744eca194e2f242f82f
smilekoko <179621228@qq.com>
2025-04-09 17:32:12 +0800

    ...

--- app/src/main/java/com/shiki/kokocomposenew/component/page/HorizontalPagerGuide.kt
+++ app/src/main/java/com/shiki/kokocomposenew/component/page/HorizontalPagerGuide.kt
@@ -2,16 +2,22 @@ package com.shiki.kokocomposenew.component.page
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,22 +30,49 @@ import com.shiki.kokocomposenew.R
 fun HorizontalPagerExample() {
 
     val pagerState = rememberPagerState(pageCount = { 10 })
-    HorizontalPager(
-        modifier = Modifier
-            .background(Color.White)
-            .fillMaxWidth(),
-        state = pagerState
-    ) { page ->
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+    Column(
+        modifier = Modifier.background(Color.White)
+    ) {
+
+        HorizontalPager(
             modifier = Modifier
                 .background(Color.White)
-                .padding(16.dp)
+                .fillMaxWidth(),
+            state = pagerState
+        ) { page ->
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text("$page page")
+                Image(painter = painterResource(R.drawable.pika), "")
+            }
+        }
+
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
         ) {
-            Text("$page page")
-            Image(painter = painterResource(R.drawable.pika), "")
+            repeat(pagerState.pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(16.dp)
+                )
+            }
+
         }
     }
+
 
 }
