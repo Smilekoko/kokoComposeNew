2029b30a692963747418c9c441a25015430c5b2c
smilekoko <179621228@qq.com>
2025-04-10 16:17:31 +0800

    ...

--- app/src/main/java/com/shiki/kokocomposenew/animation/TextMotionAnimatedGuide.kt
+++ app/src/main/java/com/shiki/kokocomposenew/animation/TextMotionAnimatedGuide.kt
@@ -1,22 +1,35 @@
 package com.shiki.kokocomposenew.animation
 
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * 文本缩放动画
@@ -33,18 +46,54 @@ fun TextMotionAnimatedExample() {
         animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
         label = "scale"
     )
-    Box(modifier = Modifier.fillMaxSize()) {
+    val animatedColor by infiniteTransition.animateColor(
+        initialValue = Color(0xFF60DDAD),
+        targetValue = Color(0xFF4285F4),
+        animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
+        label = "color"
+    )
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        // 定义一个变量来存储 Text 的高度
+        val (textHeight, setTextHeight) = remember { mutableStateOf(0.dp) }
+        // 获取 LocalDensity
+        val density = LocalDensity.current
         Text(
             "Hello",
             modifier = Modifier
+                .padding(top = 36.dp)
+                .align(Alignment.CenterHorizontally)
                 .graphicsLayer {
                     scaleX = scale
                     scaleY = scale
                     transformOrigin = TransformOrigin.Center
                 }
-                .align(Alignment.Center),
+                .layout { measurable, constraints ->
+
+                    val placeable = measurable.measure(constraints)
+
+                    setTextHeight(with(density) { placeable.height.toDp() })
+
+                    layout(placeable.width, placeable.height) {
+                        placeable.placeRelative(0, 0)
+                    }
+                },
             style = LocalTextStyle.current.copy(textMotion = TextMotion.Animated)
         )
+
+        BasicText(
+            "Hello Compose", modifier =
+            Modifier
+                .padding(top = textHeight + 16.dp),
+            style = TextStyle(
+                color = animatedColor, fontSize = 24.sp
+            )
+        )
     }
+
+
 }
