471f8f354439ecaa48157352baf995bec43174eb
smilekoko <179621228@qq.com>
2025-04-21 12:11:50 +0800

    ...

--- app/src/main/java/com/shiki/kokocomposenew/component/badge/BadgedBoxGuide.kt
+++ app/src/main/java/com/shiki/kokocomposenew/component/badge/BadgedBoxGuide.kt
@@ -1,10 +1,21 @@
 package com.shiki.kokocomposenew.component.badge
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun BadgedBoxExample() {
+    var itemCount by remember { mutableStateOf(0) }
 
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+
+    }
 }
