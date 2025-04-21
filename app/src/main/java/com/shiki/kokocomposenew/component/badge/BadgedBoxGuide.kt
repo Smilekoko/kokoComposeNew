a2f9ad30d53d60f94bc75162a4abbe74f43afcfc
smilekoko <179621228@qq.com>
2025-04-21 11:12:35 +0800

    ...

---
+++ app/src/main/java/com/shiki/kokocomposenew/component/badge/BadgedBoxGuide.kt
@@ -0,0 +1,10 @@
+package com.shiki.kokocomposenew.component.badge
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun BadgedBoxExample() {
+
+}
